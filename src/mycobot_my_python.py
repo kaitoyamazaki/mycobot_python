4b9bd1746661a891481c705f4116aaf294b9431b
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-12-30 17:37:10 +0900

    エンコーダの値を出力させるテスト

--- src/mycobot_my_python.py
+++ src/mycobot_my_python.py
@@ -67,7 +67,7 @@ class ApiTest():
         for i in range(len(data)):
             now_data = data[i]
             self.mc.send_angles([0.0, now_data[0], now_data[1], now_data[2], 0.0, -45], 100)
-            angles = self.mc.get_encoders()
+            angles = self.mc.get_angles()
             print(f"angles is {angles}")
             time.sleep(0.1)
 
