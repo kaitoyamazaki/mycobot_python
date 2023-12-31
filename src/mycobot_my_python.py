ec4708d26913570bbb71ea7ae6e4abdc6991f4df
kaitoyamazaki <reflect018@yahoo.co.jp>
2023-12-31 10:31:33 +0900

    速度を変更

--- src/mycobot_my_python.py
+++ src/mycobot_my_python.py
@@ -64,7 +64,7 @@ class ApiTest():
         for i in range(len(data)):
             now_data = data[i]
             self.mc.send_angles([0.0, now_data[0], now_data[1], now_data[2], 0.0, -45], 75)
-            angles = self.mc.get_angles()
+            #angles = self.mc.get_angles()
             #print(f"angles is {angles}")
             #time.sleep(0.01)
 
